9c0697125f1d9ec32b87115f0042f580740e5a3a
Mohak Bhambry <MBhambry@users.noreply.github.com>
2014-07-19 16:32:32 +0530

    Update costFunction.m

--- Machine-Learning/ex2/costFunction.m
+++ Machine-Learning/ex2/costFunction.m
@@ -27,3 +27,23 @@ grad = zeros(size(theta));
 H = sigmoid (X *theta);
 T = y.*log(H) + (1 - y).*log(1 - H);
 J = -1/m*sum(T);
+
+% Submission for cost function is here
+H = sigmoid (X *theta);
+T = y.*log(H) + (1 - y).*log(1 - H);
+J = -1/m*sum(T);
+
+
+
+%submission for gradient descent is here
+for i  = 1 : m,
+     	grad = grad + (H(i) - y(i)) * X(i,:)';
+end
+
+grad = 1/m*grad;
+        
+
+
+% =============================================================
+
+end
