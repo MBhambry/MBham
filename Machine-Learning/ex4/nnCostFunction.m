7af735deba517ddb3b23f429c04ab34a424db7f2
Mohak Bhambry <MBhambry@users.noreply.github.com>
2014-08-04 11:19:28 +0530

    Update nnCostFunction.m

--- Machine-Learning/ex4/nnCostFunction.m
+++ Machine-Learning/ex4/nnCostFunction.m
@@ -1,3 +1,5 @@
+% Author - Mohak Bhambry
+
 function [J grad] = nnCostFunction(nn_params, ...
                                    input_layer_size, ...
                                    hidden_layer_size, ...
@@ -62,42 +64,73 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
- % forward propagation
-y = eye(num_labels)(y,:);
+% Add ones to the X data matrix 
+    	X = [ones(m,1) X];
 
-a_1 = [ones(m,1) X];
 
-z_2 = (Theta1*a_1')';
+	a2 = sigmoid(Theta1 * X');
 
-a_2 = sigmoid(Theta1*a_1')';
-a_2 = [ones(size(a_2,1),1) a_2];
-a_3 = sigmoid(Theta2*a_2')';
+	a2 = [ones(m,1) a2'];
 
-J_reg = lambda/(2*m)*(sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
-J = (1/m)*sum(sum(-y.*log(a_3) - (1-y).*log(1-a_3))) + J_reg; 
+	h_theta = sigmoid(Theta2 * a2'); % h_theta equals z3
 
- 
+	yk = zeros(num_labels, m); 
 
-% backward propagation
-d_3 = a_3 - y;
-d_2 = ((d_3*Theta2(:,2:end)).*sigmoidGradient(z_2));
+	for i=1:m,
+  	
+  	yk(y(i),i)=1;
 
-Theta1_grad = 1/m * d_2' * a_1;
-Theta2_grad = 1/m * d_3' * a_2;
+	end
 
-%add regularization:
 
-Theta1_grad(:,2:end) = Theta1_grad(:,2:end) + lambda/m*Theta1(:,2:end);
-Theta2_grad(:,2:end) = Theta2_grad(:,2:end) + lambda/m*Theta2(:,2:end);
+	J = (1/m) * sum ( sum (  (-yk) .* log(h_theta)  -  (1-yk) .* log(1-h_theta) ));
 
 
 
+	t1 = Theta1(:,2:size(Theta1,2));
+	t2 = Theta2(:,2:size(Theta2,2));
+
+	Reg = lambda  * (sum( sum ( t1.^ 2 )) + sum( sum ( t2.^ 2 ))) / (2*m);
+
+	J = J + Reg;
 
 
 
+	for t=1:m,
+  
+  	a1 = X(t,:); 
+  	z2 = Theta1 * a1';
+  
+  	a2 = sigmoid(z2);
+  	a2 = [1 ; a2]; % add bias
+  
+  	z3 = Theta2 * a2;
+  
+  	a3 = sigmoid(z3); % final activation layer a3 == h(theta)
+  
+  
+  	z2=[1; z2];
+  
+  	delta_3 = a3 - yk(:,t);
+  	delta_2 = (Theta2' * delta_3) .* sigmoidGradient(z2);
+  
+  	delta_2 = delta_2(2:end); 
+  
+  	Theta1_grad = Theta1_grad + delta_2 * a1; 
+  	Theta2_grad = Theta2_grad + delta_3 * a2';
+
+	end;
 
 
+	Theta1_grad(:, 1) = Theta1_grad(:, 1) ./ m;
 
+	Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) ./ m + ((lambda/m) * Theta1(:, 2:end));
+
+
+	Theta2_grad(:, 1) = Theta2_grad(:, 1) ./ m;
+
+	Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) ./ m + ((lambda/m) * Theta2(:, 2:end));
+    
 
 
 
